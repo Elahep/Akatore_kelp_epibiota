0da8dc6d7a03cd30ce7984c84b1b840aade673df
Elahe Parvizi <Elahep@users.noreply.github.com>
2021-07-02 12:56:31 +1200

    Create model4.sh

---
+++ delimitR/non-uplift topology models/O. neglectus/model4/model4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account=XXXX
+#SBATCH --job-name=model4_Onithochiton
+#SBATCH --partition=large
+#SBATCH --time=25:00:00
+#SBATCH --mem=10G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=XXXX
+#SBATCH --cpus-per-task=8
+
+
+./fsc26 -t model4.tpl -e model4.est -m -n500000 -L40 -c8 -B12 --removeZeroSFS -M --multiSFS
